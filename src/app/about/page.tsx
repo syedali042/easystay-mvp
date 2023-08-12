08648b6dd6c8c6558570ff499868bdc2a8bf4d4d
Syed <salizahid75@gmail.com>
2023-08-13 03:55:11 +0500

    Fix: Subscribe section error

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,12 @@
-import rightImg from "@/images/about-hero-right.png";
-import React, { FC } from "react";
-import SectionFounder from "./SectionFounder";
-import SectionStatistic from "./SectionStatistic";
-import SectionHero from "./SectionHero";
-import BgGlassmorphism from "@/components/BgGlassmorphism";
-import BackgroundSection from "@/components/BackgroundSection";
-import SectionClientSay from "@/components/SectionClientSay";
-import SectionSubscribe2 from "@/components/SectionSubscribe2";
+import rightImg from '@/images/about-hero-right.png';
+import React, {FC} from 'react';
+import SectionFounder from './SectionFounder';
+import SectionStatistic from './SectionStatistic';
+import SectionHero from './SectionHero';
+import BgGlassmorphism from '@/components/BgGlassmorphism';
+import BackgroundSection from '@/components/BackgroundSection';
+import SectionClientSay from '@/components/SectionClientSay';
+import SectionSubscribe2 from '@/components/SectionSubscribe2';
 
 export interface PageAboutProps {}
 
