87738824ad98338b01876fdd3c453a180ca8f864
Syed <salizahid75@gmail.com>
2023-08-14 23:06:35 +0500

    Fix: missing component

--- src/app/(client-components)/(HeroSearchForm2Mobile)/HeroSearchForm2MobileFactory.tsx
+++ src/app/(client-components)/(HeroSearchForm2Mobile)/HeroSearchForm2MobileFactory.tsx
@@ -3,16 +3,11 @@
 import React from 'react';
 import {PathName} from '@/routers/types';
 import HeroSearchForm2Mobile from './HeroSearchForm2Mobile';
-import HeroSearchForm2RealEstateMobile from './HeroSearchForm2RealEstateMobile';
 import {usePathname} from 'next/navigation';
 
 const PAGES_REAL_ESTATE: PathName[] = ['/', '/', '/'];
 
 const HeroSearchForm2MobileFactory = () => {
-  const pathname = usePathname();
-  if (PAGES_REAL_ESTATE.includes(pathname as PathName)) {
-    return <HeroSearchForm2RealEstateMobile />;
-  }
   return <HeroSearchForm2Mobile />;
 };
 
