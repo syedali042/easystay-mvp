56d20ca80ba42815e87a51887f6962e88e77a0f1
Syed <salizahid75@gmail.com>
2023-08-20 13:06:26 +0500

    Fix: tour details description

--- src/app/tours/[id]/Itinerary.js
+++ src/app/tours/[id]/Itinerary.js
@@ -1,5 +1,6 @@
 import {getTourById} from '@/redux/slices/tours';
 import {useSelector} from 'react-redux';
+import parse from 'html-react-parser';
 
 const TourItinerary = ({tourId}) => {
   const tour = useSelector(getTourById({tourId}));
@@ -8,7 +9,7 @@ const TourItinerary = ({tourId}) => {
       <h2 className="text-2xl font-semibold">Experience Description</h2>
       <div className="w-14 border-b border-neutral-200 dark:border-neutral-700"></div>
       <div className="text-neutral-6000 dark:text-neutral-300">
-        {tour?.description}
+        {parse(tour?.description)}
       </div>
     </div>
   );
